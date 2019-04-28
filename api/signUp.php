a71042946da7ac33d697ba4394a55a5ab54a63e3
Jessica Rios <jerios@csumb.edu>
2019-04-28 02:34:11 +0000

    v1 hashing

--- api/signUp.php
+++ api/signUp.php
@@ -4,8 +4,15 @@
     
     $conn = getDatabaseConnection("get_me_there");
     
-    $sql = "SELECT * FROM users";
+    $options = [
+        'cost' => 11,
+        ];
+        
+    $hashedPassword = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
     
+    $sql = "INSERT INTO user (name, birthday, username, email, password, mpg)" . 
+           "VALUES (:name, :birthday, :username, :email, :hashedPassword, :mpg)";
+           
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
