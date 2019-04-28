814ff87343d87de99ba55cc80e364a2a1e8093bd
Frank Duenez <fduenez@csumb.edu>
2019-04-28 02:51:14 +0000

    edited signUp.php to check if fields were left empty

--- api/signUp.php
+++ api/signUp.php
@@ -1,21 +1,103 @@
 <?php
 
     include '../dbConnection.php';
+    switch($httpMethod) {
+    case "OPTIONS":
+      header("Access-Control-Allow-Headers: X-ACCESS_TOKEN, Access-Control-Allow-Origin, Authorization, Origin, X-Requested-With, Content-Type, Content-Range, Content-Disposition, Content-Description");
+      header("Access-Control-Allow-Methods: POST, GET");
+      header("Access-Control-Max-Age: 3600");
+      exit();
+    case "GET":
+      http_response_code(401);
+      echo "Not Supported";
+      break;
+    case 'POST':
+      header("Access-Control-Allow-Origin: *");
+      
+      header("Content-Type: application/json");
+
+      
+      $rawJsonString = file_get_contents("php://input");
+
+      
+      $jsonData = json_decode($rawJsonString, true);
+
+      
+      if (empty($_POST["password"])) {
+        echo json_encode(array(
+          "isSignedUp" => false, 
+          "message" => "No password provided"));
+          
+        exit;
+      }
+
+      if (empty($_POST["confirmation"])) {
+        echo json_encode(array(
+          "isSignedUp" => false, 
+          "message" => "No password confirmation provided"));
+          
+        exit;
+      }
+
+      if ($_POST["password"] != $_POST["confirmation"]) {
+        echo json_encode(array(
+          "isSignedUp" => false, 
+          "message" => "password does not equal confirmation"));
+          
+        exit;
+      }
     
     $conn = getDatabaseConnection("get_me_there");
     
     $options = [
         'cost' => 11,
         ];
+    try
+    {
         
-    $hashedPassword = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
-    
-    $sql = "INSERT INTO user (name, birthday, username, email, password, mpg)" . 
-           "VALUES (:name, :birthday, :username, :email, :hashedPassword, :mpg)";
-           
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    echo json_encode($records);
+        
+        $hashedPassword = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
+        
+        $sql = "INSERT INTO user (name, birthday, username, email, password, mpg)" . 
+               "VALUES (:name, :birthday, :username, :email, :hashedPassword, :mpg)";
+               
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(array (
+          ":email" => $_POST['email'],
+          ":hashedPassword" => $hashedPassword));
+        
+        $_SESSION["email"] = $record["email"];
+        $_SESSION["isAdmin"] = false;
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        echo json_encode($records);
+    }
+    catch (PDOException $ex) {
+        switch ($ex->getCode()) {
+          case "23000":
+            echo json_encode(array(
+              "isSignedUp" => false, 
+              "message"=> "email taken, try another",
+              "details" => $ex->getMessage()));
+            break;
+          default:
+            echo json_encode(array(
+              "isSignedUp" => false, 
+              "message"=> $ex->getMessage(),
+              "details" => $ex->getMessage()));
+            break;
+        }
+        exit;
+      }
+      break;
+    case 'PUT':
+      // TODO: Access-Control-Allow-Origin
+      http_response_code(401);
+      echo "Not Supported";
+      break;
+    case 'DELETE':
+      // TODO: Access-Control-Allow-Origin
+      http_response_code(401);
+      break;
+  }
 ?>
