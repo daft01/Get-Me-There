a8128f0a576cdbe0f084cc7cc4bebadfbc3d425b
Iris Manriquez <imanriquez@csumb.edu>
2019-05-05 01:19:46 +0000

    test numero dos email

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,23 +1,12 @@
 <?php
 
-         $to = "johanamanriquez10@gmail.com";
-         $subject = "This is subject";
-         
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
-         
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
+       $from = "test@hostinger-tutorials.com";
+    $to = "test@gmail.com";
+    $subject = "Checking PHP mail";
+    $message = "PHP mail works just fine";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
 
 // require_once('connect.php');
 // // require('config.php');
