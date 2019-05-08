fc4b75506dda36aa18ec6f90e2a8226d9bf381be
Frank Duenez <fduenez@csumb.edu>
2019-05-08 02:26:39 +0000

    testing database connection to cars

--- car.php
+++ car.php
@@ -1,11 +1,10 @@
 <?php
 
-    include 'dbConnection.php';
+    include 'connect.php';
     
-    $conn = getDBConnection();
+    $conn = getDBConnection()();
     
     $sql = "INSERT INTO `cars`(`email`, `model`, `year`, `highway`, `city`, `made`) VALUES ('hello123@gmail.com', '". $_POST['model'] . "', " . $_POST['year'] . ", " . $_POST['highway'] . ", " . $_POST['city'] . " , '" . $_POST['make'] . "')";
-    echo $sql;
     $stmt = $conn->prepare($sql);
     $stmt->execute();
 ?>
