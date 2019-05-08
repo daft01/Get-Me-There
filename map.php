39a4e1240990e4871aaded29d0bcfb1677c5f637
Daniel Morales <danmorales@csumb.edu>
2019-05-08 18:30:54 +0000

    lol

--- map.php
+++ map.php
@@ -193,15 +193,18 @@
         </div>
       </div>
     </div>
-      <div id = "email">
-      <?= $email ?>
-    </div>
+      <div id = "email"></div>
     
-      <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0vzljkSDp7Hk3CWIytTfZJEM8jS-UooU&libraries=places"></script>
     <script type="text/javascript" src="javascript/googleMapsCode.js"></script>
-    <script type="text/javascript" src="javascript/login.js"></script>
+    
+    <script >
+        var email = "<?php echo $email?>";
+        document.getElementById("email").innerHTML = email;
+    </script>
+    
   </body>
 </html>
