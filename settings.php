a19c7f410d146138db6dd8d984352ba798f265f0
Frank Duenez <fduenez@csumb.edu>
2019-05-08 21:18:50 +0000

    hello

--- settings.php
+++ settings.php
@@ -53,6 +53,10 @@
           <button type = button id = "button" class="btn btn-outline-success">Update Car</button>
           <button type = button id="deleteButton" class="btn btn-outline-danger"> Back</button>
           <div id="container"> </div>
+          
+          <div id="email" style="display:none;">
+            
+          </div>
             
         </fieldset>
       </form>
@@ -60,19 +64,16 @@
   </div>
   
   <script >
-        var email = "<?php echo $email?>";
-        
+          var email = "<?php echo $email?>";
+          
         document.getElementById("email").innerHTML = email;
         
-        if(document.getElementById("email").innerHTML == ""){
-          document.getElementById("login").innerHTML = "<a href='signIn.php'>Sign In</a>";
-        }
+  
     </script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="javascript/login.js"></script>
   <script>
         /* global $ */
         var model;
@@ -189,7 +190,7 @@
         
         $("#deleteButton").on("click", function()
         {
-          window.location = "../Get-Me-There/index.php"
+          window.location = "../Get-Me-There/map.php"
         });
         var email = "<?php echo $email?>";
         $("#button").on("click", function()
@@ -206,7 +207,7 @@
                "make": make,
                "model": model,
                "year": $("#year").val(),
-               "email":document.getElementById("email").innerHTML,
+               "email":document.getElementById('email').innerHTML,
             },
             success:function(data, status)
             {
